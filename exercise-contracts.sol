5b6454fd1d38587b0e38d4c07bea322a89f11393
deji-ice <ayodejiatanda1@gmail.com>
2024-11-13 09:34:44 +0100

    tests

---
+++ exercise-contracts.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.26;
+
+contract SimpleStorage {
+    // write all the code here
+    uint storedData = 10;
+    string name = "John";
+    bool switchOn = true;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint){
+        return storedData;
+    }
+}
